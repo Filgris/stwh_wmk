5838f49f0bf4ae78bf3f6663c2d82bd25e4f1eea
Filgris <124512680+Filgris@users.noreply.github.com>
2024-04-28 16:26:08 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='stwh_wmk_pack',
+	version='0.1.2',
+	description='stwh_wmk_package',
+	url='https://github.com/Filgris/stwh_wmk.git',
+	author='neu3128',
+	author_email='neu3128@gmail.com',
+	license='neu3128',
+	packages=['stwh_wmk_pack'],
+	zip_safe=False
+)
